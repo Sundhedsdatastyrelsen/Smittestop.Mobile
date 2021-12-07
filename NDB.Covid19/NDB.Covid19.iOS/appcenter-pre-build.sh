30c1011b91fe99579b5feb54891f1e0591a1f859
Pavel Shramau <pavels@netcompany.com>
2021-12-07 11:18:18 +0100

    Added appcenter-pre-build.sh

---
+++ NDB.Covid19/NDB.Covid19.iOS/appcenter-pre-build.sh
@@ -0,0 +1,44 @@
+﻿#!/bin/bash
+#
+# Src: https://github.com/microsoft/appcenter/blob/master/sample-build-scripts/xamarin/app-constants/appcenter-pre-build.sh
+#   
+#    MIT License
+#
+#    Copyright (c) Microsoft Corporation. All rights reserved.
+#
+#    Permission is hereby granted, free of charge, to any person obtaining a copy
+#    of this software and associated documentation files (the "Software"), to deal
+#    in the Software without restriction, including without limitation the rights
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#    copies of the Software, and to permit persons to whom the Software is
+#    furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in all
+#    copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE
+#
+# For Xamarin, change some constants located in some class of the app.
+# In this sample, suppose we have an AppConstant.cs class in shared folder with follow content:
+#
+# namespace Core
+# {
+#     public class AppConstant
+#     {
+#         public const string ApiUrl = "https://CMS_MyApp-Eur01.com/api";
+#     }
+# }
+# 
+# Suppose in our project exists two branches: master and develop. 
+# We can release app for production API in master branch and app for test API in develop branch. 
+# We just need configure this behaviour with environment variable in each branch :)
+# 
+# The same thing can be perform with any class of the app.
+#
+# AN IMPORTANT THING: FOR THIS SAMPLE YOU NEED DECLARE API_URL ENVIRONMENT VARIABLE IN APP CENTER BUILD CONFIGURATION.
