6d4960dd66561df69a1cf8bb4e3f8562e825f7bb
Pavel Shramau <pavels@netcompany.com>
2021-12-07 12:41:56 +0100

    Added env var injection from app center

--- NDB.Covid19/NDB.Covid19.iOS/appcenter-pre-build.sh
+++ NDB.Covid19/NDB.Covid19.iOS/appcenter-pre-build.sh
@@ -99,3 +99,43 @@ then
 fi
 
 CONF_FILE=$APPCENTER_SOURCE_DIRECTORY/NDB.Covid19/NDB.Covid19/config.json
+
+if [ -e "$CONF_FILE" ]
+then
+    echo "Updating BaseUrl to $BaseUrl in config.json"
+    sed -i '' 's#BaseUrl = "[-A-Za-z0-9:_./]*"#BaseUrl = "'$BaseUrl'"#' $CONF_FILE
+
+    echo "Updating AuthHeader to $AuthHeader in config.json"
+    sed -i '' 's#AuthHeader => "[-A-Za-z0-9:_./]*"#AuthHeader => "'$AuthHeader'"#' $CONF_FILE
+
+    echo "Updating FetchMinMinutes to $FetchMinMinutes in config.json"
+    sed -i '' 's#FetchMinMinutes => "[-A-Za-z0-9:_./]*"#FetchMinMinutes => "'$FetchMinMinutes'"#' $CONF_FILE
+
+    echo "Updating Oauth2ClientId to $Oauth2ClientId in config.json"
+    sed -i '' 's#Oauth2ClientId = [-A-Za-z0-9:_./]*#Oauth2ClientId = '$Oauth2ClientId'#' $CONF_FILE
+
+    echo "Updating Oauth2Scope to $Oauth2Scope in config.json"
+    sed -i '' 's#Oauth2Scope = "[-A-Za-z0-9:_./]*"#Oauth2Scope = "'$Oauth2Scope'"#' $CONF_FILE
+
+    echo "Updating Oauth2RedirectUrl to $Oauth2RedirectUrl in config.json"
+    sed -i '' 's#Oauth2RedirectUrl = "[-A-Za-z0-9:_./]*"#Oauth2RedirectUrl = "'$Oauth2RedirectUrl'"#' $CONF_FILE
+
+    echo "Updating Oauth2AuthoriseUrl to $Oauth2AuthoriseUrl in config.json"
+    sed -i '' 's#Oauth2AuthoriseUrl = "[-A-Za-z0-9:_./]*"#Oauth2AuthoriseUrl = "'$Oauth2AuthoriseUrl'"#' $CONF_FILE
+
+    echo "Updating Oauth2AccessTokenUrl to $Oauth2AccessTokenUrl in config.json"
+    sed -i '' 's#Oauth2AccessTokenUrl = "[-A-Za-z0-9:_./]*"#Oauth2AccessTokenUrl = "'$Oauth2AccessTokenUrl'"#' $CONF_FILE
+
+    echo "Updating Oauth2VerifyTokenPublicKey to $Oauth2VerifyTokenPublicKey in config.json"
+    sed -i '' 's#Oauth2VerifyTokenPublicKey = "[-A-Za-z0-9:_./]*"#Oauth2VerifyTokenPublicKey = "'$Oauth2VerifyTokenPublicKey'"#' $CONF_FILE
+
+    echo "Updating UseDevTools to $UseDevTools in config.json"
+    sed -i '' 's#UseDevTools = "[-A-Za-z0-9:_./]*"#UseDevTools = "'$UseDevTools'"#' $CONF_FILE
+   
+    echo "File content:"
+    cat $CONF_FILE
+    echo "Finished updating $CONF_FILE"
+else
+    echo "$CONF_FILE is missing"
+    exit 1
+fi
