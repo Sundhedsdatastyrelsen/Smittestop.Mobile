e1caf79e83d9e68c2a23134e001142a8f9b80e21
Juliehoejrisp <juhp@netcompany.com>
2022-02-25 14:23:00 +0100

    Build of ui farwell page
    Build of ui farwell page

---
+++ NDB.Covid19/NDB.Covid19.iOS/Views/FarwellSmittestopPage/FarwellSmitestopPageViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using UIKit;
+
+namespace NDB.Covid19.iOS.Views.FarwellSmittestopPage
+{
+    public partial class FarwellSmittestopPageViewController : UIViewController
+    {
+        public FarwellSmittestopPageViewController() : base("FarwellSmittestopPageViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
