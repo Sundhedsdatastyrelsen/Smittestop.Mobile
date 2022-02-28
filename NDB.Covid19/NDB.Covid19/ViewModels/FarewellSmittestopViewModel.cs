a3edddfe6a14af5195ddd4ef53fa96b0e7805ae9
NC-001 <86482015+NC-001@users.noreply.github.com>
2022-02-28 10:00:08 +0100

    Created new Farewell Smittestop screen for Android (#24)

---
+++ NDB.Covid19/NDB.Covid19/ViewModels/FarewellSmittestopViewModel.cs
@@ -0,0 +1,13 @@
+﻿using I18NPortable;
+
+namespace NDB.Covid19.ViewModels
+{
+    public class FarewellSmittestopViewModel
+    {
+        public static string FAREWELL_SMITTESTOP_TITLE => "FAREWELL_SMITTESTOP_TITLE".Translate();
+        public static string FAREWELL_SMITTESTOP_BODY_ONE => "FAREWELL_SMITTESTOP_BODY_ONE".Translate();
+        public static string FAREWELL_SMITTESTOP_BODY_TWO => "FAREWELL_SMITTESTOP_BODY_TWO".Translate();
+        public static string FAREWELL_SMITTESTOP_BODY_THREE => "FAREWELL_SMITTESTOP_BODY_THREE".Translate();
+        public static string FAREWELL_SMITTESTOP_BUTTON_TEXT => "FAREWELL_SMITTESTOP_BUTTON_TEXT".Translate();
+    }
+}
