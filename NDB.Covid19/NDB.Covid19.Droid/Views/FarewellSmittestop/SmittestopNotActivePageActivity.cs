d957ec6e055c010de1557eb77e890e0611645333
NC-001 <86482015+NC-001@users.noreply.github.com>
2022-03-02 08:41:05 +0100

    UI for Smittestop not active on Android

--- NDB.Covid19/NDB.Covid19.Droid/Views/FarewellSmittestop/SmittestopNotActivePageActivity.cs
+++ NDB.Covid19/NDB.Covid19.Droid/Views/FarewellSmittestop/SmittestopNotActivePageActivity.cs
@@ -18,6 +18,12 @@ namespace NDB.Covid19.Droid.Views.FarewellSmittestop
             TextView infoText = FindViewById<TextView>(Resource.Id.information_text);
             infoText.Text = SMITTESTOP_NOT_ACTIVE_TEXT;
 
+            TextView moreInfoText = FindViewById<TextView>(Resource.Id.more_info_text);
+            moreInfoText.Text = SMITTESTOP_NOT_ACTIVE_MORE_INFO;
+
+            TextView moreInfoURL = FindViewById<TextView>(Resource.Id.more_info_link);
+            moreInfoURL.Text = SMITTESTOP_NOT_ACTIVE_INFO_LINK;
+
             Button button = FindViewById<Button>(Resource.Id.language_button);
             button.Text = SMITTESTOP_NOT_ACTIVE_BUTTONT_TEXT;
         }
--- NDB.Covid19/NDB.Covid19/ViewModels/SmittestopNotActiveViewModel.cs
+++ NDB.Covid19/NDB.Covid19/ViewModels/SmittestopNotActiveViewModel.cs
@@ -8,5 +8,6 @@ namespace NDB.Covid19.ViewModels
         public static string SMITTESTOP_NOT_ACTIVE_TEXT => "SMITTESTOP_NOT_ACTIVE_TEXT".Translate();
         public static string SMITTESTOP_NOT_ACTIVE_MORE_INFO => "SMITTESTOP_NOT_ACTIVE_MORE_INFO".Translate();
         public static string SMITTESTOP_NOT_ACTIVE_BUTTONT_TEXT => "SMITTESTOP_NOT_ACTIVE_BUTTONT_TEXT".Translate();
+        public static string SMITTESTOP_NOT_ACTIVE_INFO_LINK => "SMITTESTOP_NOT_ACTIVE_INFO_LINK".Translate();
     }
 }
