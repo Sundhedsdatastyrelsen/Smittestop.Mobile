ad3ee8f2128d386b2f11fce4374f94944f7ba158
NC-204 <NC-204@protonmail.com>
2022-03-22 10:34:58 +0100

    Added view model with new texts.

--- NDB.Covid19/NDB.Covid19/ViewModels/FarewellSmittestopViewModel.cs
+++ NDB.Covid19/NDB.Covid19/ViewModels/FarewellSmittestopViewModel.cs
@@ -9,5 +9,9 @@ namespace NDB.Covid19.ViewModels
         public static string FAREWELL_SMITTESTOP_BODY_TWO => "FAREWELL_SMITTESTOP_BODY_TWO".Translate();
         public static string FAREWELL_SMITTESTOP_BODY_THREE => "FAREWELL_SMITTESTOP_BODY_THREE".Translate();
         public static string FAREWELL_SMITTESTOP_BUTTON_TEXT => "FAREWELL_SMITTESTOP_BUTTON_TEXT".Translate();
+
+        public static string SMITTESTOP_NOT_ACTIVE_MORE_INFO => "SMITTESTOP_NOT_ACTIVE_MORE_INFO".Translate();
+        public static string SMITTESTOP_NOT_ACTIVE_INFO_LINK_TEXT => "SMITTESTOP_NOT_ACTIVE_INFO_LINK_TEXT".Translate();
+        public static string SMITTESTOP_NOT_ACTIVE_INFO_URL => "SMITTESTOP_NOT_ACTIVE_INFO_URL".Translate();
     }
 }
